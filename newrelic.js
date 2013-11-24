3138fcbc475ebc3f3906d4b2ffef3c916e502bbf
Roberto Rodriguez <roberto@rodriguezartavia.com>
2013-11-23 20:26:24 -0600

    new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Rodco-ERP-Stage'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'e03a8e8b6a8f3b01c5483cabc675a595d1cf1416',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
